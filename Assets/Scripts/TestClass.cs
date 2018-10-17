503fd0f72fa072d7d5c6814efe294a600178534a
peke2 <abem@dc4.so-net.ne.jp>
2018-10-17 17:04:08 +0900

    最初の構成をコミット。

---
+++ Assets/Scripts/TestClass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class TestClass {
+    public TestClass()
+    {
+        var clickStream = Observable.EveryUpdate().Where(_ => Input.GetMouseButtonDown(0));
+        //  コルーチンを意識しなくても非同期の処理を扱える
+        //  MonoBehaviourじゃなくても使える(UniRxがコルーチン経由で呼び出してくれている)
+        clickStream.Subscribe(_ => {
+            Debug.Log("Button down");
+            func();
+        });
+    }
+
+    void func()
+    {
+        Debug.Log("関数呼び出し");
+    }
+
+}
---
+++ Assets/Scripts/TestUniRX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UniRx;
+
+public class TestUniRX : MonoBehaviour {
+
+    TestClass tc;
+
+	// Use this for initialization
+	void Start () {
+        //var clickStream = Observable.EveryUpdate().Where(_ => Input.GetMouseButtonDown(0));
+        //clickStream.Subscribe(_ =>{
+        //    Debug.Log("Button down");
+        //    func();
+        //});
+        tc = new TestClass();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //void func()
+    //{
+    //    Debug.Log("関数呼び出し");
+    //}
+}
